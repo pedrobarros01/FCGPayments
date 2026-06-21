c197252b1dbbb8cebd1781f44eb806001c8ba142
Pedro Barros <pedrobarros0101@outlook.com>
2026-06-20 22:14:18 -0300

    feat: create worker to payments flow

--- src/FCG.Payments/Program.cs
+++ src/FCG.Payments/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+
+using FCG.Payments;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+IHost host = builder.Build();
+host.Run();
---
+++ src/FCG.Payments/Worker.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCG.Payments;
+
+public sealed class Worker(ILogger<Worker> logger) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while(!stoppingToken.IsCancellationRequested) 
+        { 
+            logger.LogInformation("Online e rodando");
+
+        }
+
+    }
+}
