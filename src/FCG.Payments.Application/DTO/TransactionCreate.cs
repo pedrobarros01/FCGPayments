bdcde3fac1d5dfa58915a4221de9ad716e7fe563
Pedro Barros <pedrobarros0101@outlook.com>
2026-06-22 01:51:40 -0300

    feat: create service paymentTransaction

---
+++ src/FCG.Payments.Application/DTO/TransactionCreate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCG.Payments.Application.DTO
+{
+    public record TransactionCreate
+    {
+        public Guid UserId { get; set; }
+        public Guid GameId { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ src/FCG.Payments.Application/Services/PaymentTransactionService.cs
@@ -0,0 +1,34 @@
+﻿using FCG.Payments.Application.DTO;
+using FCG.Payments.Application.Interfaces;
+using FCG.Payments.Domain.Entities;
+using FCG.Payments.Domain.Interfaces;
+using FCG.Payments.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCG.Payments.Application.Services;
+
+public class PaymentTransactionService : BaseApplicationService, IPaymentTransactionService
+{
+    private readonly IPaymentTransactionDomainService _paymentTransactionDomainService;
+
+    public PaymentTransactionService(IUnitOfWork unitOfWork, IPaymentTransactionDomainService paymentTransactionDomainService) : base(unitOfWork)
+    {
+        _paymentTransactionDomainService = paymentTransactionDomainService ?? throw new ArgumentNullException(nameof(paymentTransactionDomainService));
+    }
+
+    public async Task<PaymentTransaction> ProcessPayment(TransactionCreate transaction)
+    {
+        PaymentTransaction paymentTransaction = new PaymentTransaction
+        {
+            GameId = transaction.GameId,
+            Price = transaction.Price,
+            UserId = transaction.UserId
+        };
+        var transactionInserted = await _paymentTransactionDomainService.CreatePaymentTransaction(paymentTransaction);
+        await UnitOfWork.CommitAsync();
+        //Chamar função de publicar evento para notification e catalog
+        return transactionInserted;
+    }
+}
