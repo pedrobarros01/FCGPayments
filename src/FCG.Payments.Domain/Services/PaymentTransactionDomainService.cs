307b1146daf977da4f259764cf0727d15a3ae209
Pedro Barros <pedrobarros0101@outlook.com>
2026-06-21 21:54:11 -0300

    feat: create unit test to domain service the paymentTransaction

---
+++ src/FCG.Payments.Domain/Services/PaymentTransactionDomainService.cs
@@ -0,0 +1,16 @@
+﻿using FCG.Payments.Domain.Entities;
+using FCG.Payments.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCG.Payments.Domain.Services;
+
+public class PaymentTransactionDomainService : IPaymentTransactionDomainService
+{
+
+    public Task<PaymentTransaction> CreatePaymentTransaction(PaymentTransaction transaction)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ tests/FCG.Payments.Tests/DomainService/PaymentTransactionDomainServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using CommonTestUtilities.Entities;
+using FCG.Payments.Domain.Enums;
+using FCG.Payments.Domain.Services;
+using FCG.Payments.Tests.Fixture;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCG.Payments.Tests.DomainService;
+
+[Collection(nameof(PaymentTransactionFixtureCollection))]
+public class PaymentTransactionDomainServiceTest
+{
+    public PaymentTransactionBuilder _transactionFixture;
+    public PaymentTransactionDomainServiceTest(PaymentTransactionBuilder transactionFixture)
+    {
+        _transactionFixture = transactionFixture;
+    }
+    [Fact]
+    public async Task PaymentTransactionDomainService_Should_CreatePaymentTransactionWithStatus()
+    {
+        var paymentTransactionMock = _transactionFixture.GenerateTransactionWithStatusEmpty();
+        var paymentTransactionDomainService = new PaymentTransactionDomainService();
+        var paymentTransaction = await paymentTransactionDomainService.CreatePaymentTransaction(paymentTransactionMock);
+        List<Guid> statusIds = new List<Guid> { StatusOptions.Approved, StatusOptions.Reproved };
+        Assert.NotNull(paymentTransaction);
+        Assert.Contains(paymentTransaction.StatusTransaction.Id, statusIds);
+    }
+}
