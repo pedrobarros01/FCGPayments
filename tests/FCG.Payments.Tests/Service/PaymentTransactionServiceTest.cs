072a394e343e3c9c78b7d6e38bde72759fecfae1
Pedro Barros <pedrobarros0101@outlook.com>
2026-06-22 00:48:10 -0300

    feat: create unit test to payment transaction service

---
+++ tests/FCG.Payments.Tests/Service/PaymentTransactionServiceTest.cs
@@ -0,0 +1,45 @@
+﻿using CommonTestUtilities.Database;
+using FCG.Payments.Application.DTO;
+using FCG.Payments.Application.Services;
+using FCG.Payments.Domain.Entities;
+using FCG.Payments.Domain.Enums;
+using FCG.Payments.Domain.Services;
+using FCG.Payments.Infrastructure.Persistence;
+using FCG.Payments.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCG.Payments.Tests.Service;
+
+public class PaymentTransactionServiceTest
+{
+    [Fact]
+    public async Task PaymentTransactionService_Should_CreateTransactio()
+    {
+        // Arrange
+        var context = await ContextBuilder.GenerateContext();
+
+
+        var repositoryPaymentTransactionStatus = new PaymentTransactionStatusRepository(context);
+        var repositoryPaymentTransaction = new PaymentTransactionRepository(context);
+        var domainService = new PaymentTransactionDomainService(repositoryPaymentTransaction, repositoryPaymentTransactionStatus);
+        var unitOfWork = new UnitOfWork(context);
+        var service = new PaymentTransactionService(unitOfWork, domainService);
+        Random random = new Random();
+        var transactionDTO = new TransactionCreate
+        {
+            GameId = Guid.NewGuid(),
+            Price = (decimal)random.NextDouble(),
+            UserId = Guid.NewGuid()
+        };
+
+        // Act
+        var result = await service.ProcessPayment(transactionDTO);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<PaymentTransaction>(result);
+        Assert.NotNull(result.StatusTransaction);
+    }
+}
