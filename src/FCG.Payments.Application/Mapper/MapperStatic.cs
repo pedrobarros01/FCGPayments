6d9786fe547178616a595ac25f6f629a01c9f241
Pedro Barros <pedrobarros0101@outlook.com>
2026-06-23 13:51:04 -0300

    feat: create mapper static

---
+++ src/FCG.Payments.Application/Mapper/MapperStatic.cs
@@ -0,0 +1,20 @@
+﻿using FCG.Payments.Application.DTO;
+using FCG.Payments.Shared.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCG.Payments.Application.Mapper;
+
+public static class MapperStatic
+{
+    public static TransactionCreate? MapOrderPlacedEvent(OrderPlacedEvent @event)
+    {
+        return new TransactionCreate
+        {
+            UserId = @event.UserId,
+            Price = @event.Price,
+            GameId = @event.GameId,
+        };
+    }
+}
