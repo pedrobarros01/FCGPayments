004e4d3c155932832836c75f73b1073a2f140262
Pedro Barros <pedrobarros0101@outlook.com>
2026-06-23 17:55:16 -0300

    feat: create queue to consumer

---
+++ src/FCG.Payments.Application/Interfaces/IProcessingQueue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCG.Payments.Application.Interfaces;
+
+public interface IProcessingQueue<T>
+{
+    ValueTask Enqueue(T item);
+    ValueTask<T> DequeueAsync(CancellationToken cancellationToken);
+
+    int CountItems();
+}
---
+++ src/FCG.Payments.Infrastructure/Queue/ProcessingQueue.cs
@@ -0,0 +1,19 @@
+﻿using FCG.Payments.Application.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace FCG.Payments.Infrastructure.Queue;
+
+public class ProcessingQueue<T> : IProcessingQueue<T>
+{
+    private readonly Channel<T> _channel = Channel.CreateUnbounded<T>();
+
+    public int CountItems() => _channel.Reader.Count;
+
+    public ValueTask Enqueue(T item) => _channel.Writer.WriteAsync(item);
+
+    public ValueTask<T> DequeueAsync(CancellationToken cancellationToken) => _channel.Reader.ReadAsync(cancellationToken);
+}
