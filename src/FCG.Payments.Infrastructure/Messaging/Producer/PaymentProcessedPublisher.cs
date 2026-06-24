b7dd432542ee283123a9ce0bd1c6848dd3e7d789
Pedro Barros <pedrobarros0101@outlook.com>
2026-06-23 21:55:22 -0300

    feat: generate publisher

---
+++ src/FCG.Payments.Infrastructure/Messaging/Producer/PaymentProcessedPublisher.cs
@@ -0,0 +1,28 @@
+﻿using FCG.Payments.Application.Mapper;
+using FCG.Payments.Domain.Entities;
+using FCG.Payments.Domain.Interfaces;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCG.Payments.Infrastructure.Messaging.Producer;
+
+public class PaymentProcessedPublisher : IPaymentProcessedPublisher
+{
+    private readonly IPublishEndpoint _publishEndpoint;
+
+    public PaymentProcessedPublisher(IPublishEndpoint publishEndpoint)
+    {
+        _publishEndpoint = publishEndpoint;
+    }
+
+    public async Task PublishPaymentProcessed(PaymentTransaction transaction)
+    {
+        var @event = MapperStatic.MapPaymentTransactionToPaymentProcessedEvent(transaction);
+        await _publishEndpoint.Publish(@event, context =>
+        {
+            context.SetRoutingKey("payment.processed");
+        });
+    }
+}
